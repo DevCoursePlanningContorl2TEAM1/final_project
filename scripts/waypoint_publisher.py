84bd206ca4ee0edff16d2738e2bdf31c86d229f5
yemerge <bohmbohmpow@gmail.com>
2024-01-25 17:22:12 +0900

    Fixed conflict

--- scripts/waypoint_publisher.py
+++ scripts/waypoint_publisher.py
@@ -8,16 +8,6 @@ from std_msgs.msg import Empty
 from geometry_msgs.msg import PoseWithCovarianceStamped
 
 # wp_id : (x, y, yaw)
-<<<<<<< HEAD
-wp_list = {1 : (4.0, 0.0, 0.7),
-           2 : (5.23, -4.51, -0.78),
-           3 : (-5.97, -4.28, -3.14),
-           4 : (-11.56, -0.1, 0.7),
-           5 : (-7.79, -0.24, -3.14)}
-
-def talker():
-    pub_wp = rospy.Publisher('my_t3_waypoints', PoseWithCovarianceStamped, queue_size=1)
-=======
 wp_list = {1 : (4.5, -0.5, 0.7),
            2 : (5.5, -4.5, -0.78),
            3 : (-6.5, -4.2, -2.14),
@@ -26,7 +16,6 @@ wp_list = {1 : (4.5, -0.5, 0.7),
 
 def talker():
     pub_wp = rospy.Publisher('my_waypoints', PoseWithCovarianceStamped, queue_size=1)
->>>>>>> 095a39a (Created waypoint_publisher.py file.)
     pub_path_ready = rospy.Publisher('path_ready', Empty, queue_size=1)
 
     rospy.init_node('waypoint_publisher', anonymous=True)
@@ -57,11 +46,7 @@ def talker():
             if connections > 0:
                 pub_wp.publish(my_wp)
                 break
-<<<<<<< HEAD
-            rospy.loginfo("Wait for 'my_t3_waypoints' topic")
-=======
             rospy.loginfo("Wait for 'my_waypoints' topic")
->>>>>>> 095a39a (Created waypoint_publisher.py file.)
             rate.sleep()
 
         rospy.loginfo("Published waypoint number " + str(i+1))
